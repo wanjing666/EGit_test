3a354fa38ea66d0143e05d64010fd8ed38e4a192
wanjing666 <598890091@qq.com>
2017-02-27 11:41:55 +0800

    首次提交到本地仓库

---
+++ src/test01/Test20170227.java
@@ -0,0 +1,13 @@
+package test01;
+
+public class Test20170227 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
